5cabb74ed56be39161fa2864708b3fd6055c8b61
David Van Horn <dvanhorn@ccs.neu.edu>
2014-05-10 18:14:31 -0400

    Move ralist.scrbl => main.scrbl.

--- data/ralist/info.rkt
+++ data/ralist/info.rkt
@@ -1,6 +1,6 @@
 #lang setup/infotab
 (define name "Purely Functional Random-Access Lists.")
-(define scribblings '(("scribblings/ralist.scrbl" (multi-page))))
+(define scribblings '(("scribblings/main.scrbl" (multi-page))))
 (define categories '(datastructures))
 (define required-core-version "5.1.1")
 (define repositories (list "4.x"))
