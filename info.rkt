ebd4f3cde9723908ab5048cb96c7832e2534918c
David Van Horn <dvanhorn@ccs.neu.edu>
2014-05-13 10:26:31 -0400

    Dependencies.

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang setup/infotab
 
 (define collection 'multi)
-(define deps '("base" "rackunit-lib"))
-(define build-deps '("scribble-lib" "racket-doc"))
+(define deps '("base" "rackunit-lib" "rackunit-doc"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-doc"))
 
